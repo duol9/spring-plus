59e99e64b72698d4a230830c89ec1730f4f6949f
duol9 <lhyy0195@gmail.com>
2025-01-15 13:49:03 +0900

    fix: @Transactional 어노테이션 수정으로 saveTodo() 오류 해결
    - @Transactional(readOnly = true)는 데이터베이스 변화를 허용하지 않으므로, 저장 작업 시 오류가 발생.
    - saveTodo 메서드는 DB에 변화를 주는 동작이므로 @Transactional을 메서드에 별도로 적용하여 문제 해결.
    - 클래스 전체에 @Transactional을 적용하는 대신, 메서드별로 필요한 동작에 맞게 설정하도록 변경.
      - 조회 메서드(getTodo, getTodos)에는 @Transactional(readOnly = true) 유지.
      - 저장 메서드(saveTodo)에는 @Transactional(readOnly = false) 적용.

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,10 +1,14 @@
 package org.example.expert.domain.todo.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import lombok.RequiredArgsConstructor;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.request.TodosGetRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.entity.Todo;
@@ -19,12 +23,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+
+// 트랜잭셔널 없앤 이유 - (나중에 커밋할 때 메시지에 쓸거임)
+// @Transactional(readOnly = true) 해당 어노테이션의 의미는 읽기만 가능하다는 뜻으로. db 변화를 허용하지 않았음
+// saveTodo는 db에 변화를 주는 동작을 하는 메서드로 readOnly에 걸리게 되어 오류가 발생했던 것
+// 또한 클래스 전체에 @Transactional을 하는 것보단 각각의 메서드에 해주는 것이 좋다. 메서드마다 동작이 다르기 때문
+// 결론적으로 조회하는 메서드인 getTodo, getTodos에는 @Transactional(readOnly = true)을 해주고,
+// saveTodo에는 @Transactional(readOnly = false)로 변경해 읽기 전용을 비활성화했다.
 public class TodoService {
 
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
@@ -47,6 +58,8 @@ public class TodoService {
         );
     }
 
+
+    @Transactional(readOnly = true)
     public Page<TodoResponse> getTodos(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
@@ -63,6 +76,7 @@ public class TodoService {
         ));
     }
 
+    @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
